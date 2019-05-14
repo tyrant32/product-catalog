9fcd2154579c4202b54042ba3e15a72ced7f0e6d
Ivan Ivanov <tyrant32@gmail.com>
2019-05-14 10:43:53 +0300

    Products Catalog

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,15 +5,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Products</div>
 
-                    <div class="card-body">
+                <products-component></products-component>
 
-
-
-                    </div>
-                </div>
             </div>
         </div>
     </div>

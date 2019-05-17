b3930aae367c5fe2a757e437703a1a85e379d4d8
Ivan Ivanov <tyrant32@gmail.com>
2019-05-17 10:46:27 +0300

    Demo

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * Laravel - A PHP Framework For Web Artisans
@@ -21,7 +22,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../../product-catalog/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +36,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../../product-catalog/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
